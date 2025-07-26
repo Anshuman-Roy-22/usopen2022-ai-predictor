6e0968c9543cb430c7dbceea358a64f15ae90961
Anshuman Roy <anroy2209@gmail.com>
2025-07-26 18:57:41 -0400

    Update predict.py

--- predict.py
+++ predict.py
@@ -8,16 +8,16 @@ model = joblib.load("usopen_predictor_model.pkl")
 with open("model_features.json") as f:
     feature_columns = json.load(f)
 
-# Dummy example (normally you'd replace this with real input data)
-example = {
+# Mock example
+pract = {
     "elo_diff": 80,     # winner Elo - loser Elo
     "seed_diff": -2     # loser seed - winner seed
 }
 
 # Convert to DataFrame
-df = pd.DataFrame([example])[feature_columns]
+df = pd.DataFrame([pract])[feature_columns]
 
 # Predict win probability
-proba = model.predict_proba(df)[0][1]  # probability that "winner" wins
+proba = model.predict_proba(df)[0][1]  # probability calculated
 print(f"Predicted win probability: {proba:.2%}")
 
